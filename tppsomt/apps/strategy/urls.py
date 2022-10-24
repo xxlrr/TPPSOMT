a5a04844eda564ef869d3c496d5d85b3567bc231
Hongxing Hao <a1845302@adelaide.edu.au>
2022-10-25 10:44:07 +1100

    modify a little things

--- tppsomt/apps/strategy/urls.py
+++ tppsomt/apps/strategy/urls.py
@@ -6,9 +6,12 @@ Copyright (c) 2019 - present AppSeed.us
 from django.urls import path, re_path
 from apps.strategy import views
 
+
+app_name = 'strategy'
 urlpatterns = [
 
     path('constant/', views.constant, name='constant'),
+    # path('rider/', views.rider, name='rider'),
 
     # # Matches any html file
     # re_path(r'^.*\.*', views.pages, name='pages'),
--- tppsomt/core/urls.py
+++ tppsomt/core/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("", include("apps.authentication.urls")), # Auth routes - login / register
 
     # ADD NEW Routes HERE
-    path('strategy/', include(("apps.strategy.urls", "strategy"), namespace="strategy")), # A team pursuit strategy tool
+    path('strategy/', include("apps.strategy.urls")), # A team pursuit strategy tool
 
     # Leave `Home.Urls` as last the last line
     path("", include("apps.home.urls"))
